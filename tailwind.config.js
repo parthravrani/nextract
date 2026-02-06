34e7b9ebcfe6aa6fdbb0ee0f326eb51af092f25f
Miren <pmrav007@gmail.com>
2026-02-07 00:52:15 +0530

    SEO tweaks

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./**/*.md",
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./_services/**/*.md",
+    "./_apis/**/*.md",
+    "./_scrapers/**/*.md",
+    "./_case-studies/**/*.md",
+    "./_intelligence/**/*.md",
+    "./_industries/**/*.md",
+    "./blog/**/*.md",
+    "./brands/**/*.md"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
